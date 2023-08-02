893dde1ff5b8f2ef0d7e4c9df9c2cac0b4e56ed7
LiamEdwards931 <>
2023-08-02 22:15:34 +0000

    amended some of the main function text

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+
+
 def start_up():
     """
     Function for the title screen of the text game
@@ -6,10 +8,11 @@ def start_up():
     player_name = input('Please Enter Your Name:')
     print(f"Welcome To Cretaceous Forest {player_name}\n")
     print("A Text Based Game To Survive In a Cretaceous Era Forest")
+    print("To Restart The Game Just Type Restart")
     print("------------------------------")
-    input("Type Enter To Start The Game: ")
-   
-        
+    input("Press Any Key To Start The Game")
+    
+
 def main_functions():
     """
     Runs the main functions of the text based game. 
@@ -17,5 +20,6 @@ def main_functions():
     start_up()
 
 main_functions()
+restart()
 
 import classes
